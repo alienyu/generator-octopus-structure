334f617327c95fb1e583c6eedb48477ed18b84ef
alienyu <alienyu@foxmail.com>
2017-07-23 20:31:48 +0800

    add

---
+++ UI/index.js
@@ -0,0 +1,47 @@
+'use strict';
+const Generator = require('yeoman-generator');
+module.exports = class extends Generator {    // The name `constructor` is important here
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.option('skip-welcome-message', {
+            desc: 'Skips the welcome message',
+            type: Boolean
+        });
+
+        this.option('skip-install-message', {
+            desc: 'Skips the message after the installation of dependencies',
+            type: Boolean
+        });
+
+        this.option('test-framework', {
+            desc: 'Test framework to be invoked',
+            type: String,
+            defaults: 'mocha'
+        });
+
+        this.option('babel', {
+            desc: 'Use Babel',
+            type: Boolean,
+            defaults: true
+        });
+    }
+
+    method1() {
+        console.log('sub 1 just ran');
+    }
+
+    method2() {
+        console.log('sub 2 just ran');
+    }
+
+    writing() {
+        this.fs.copy(
+            this.templatePath('html/index.html'),
+            this.destinationPath('views/a.html'),
+            {
+                htmlContent: "fgdg43tgdhg"
+            }
+        )
+    }
+}
---
+++ app/index.js
@@ -0,0 +1,44 @@
+'use strict';
+const Generator = require('yeoman-generator');
+module.exports = class extends Generator {    // The name `constructor` is important here
+    constructor(args, opts) {
+        super(args, opts);
+
+        this.option('skip-welcome-message', {
+            desc: 'Skips the welcome message',
+            type: Boolean
+        });
+
+        this.option('skip-install-message', {
+            desc: 'Skips the message after the installation of dependencies',
+            type: Boolean
+        });
+
+        this.option('test-framework', {
+            desc: 'Test framework to be invoked',
+            type: String,
+            defaults: 'mocha'
+        });
+
+        this.option('babel', {
+            desc: 'Use Babel',
+            type: Boolean,
+            defaults: true
+        });
+    }
+
+    method1() {
+        console.log('method 1 just ran');
+    }
+
+    method2() {
+        console.log('method 2 just ran');
+    }
+
+    writing() {
+        this.fs.copy(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json')
+        )
+    }
+}
